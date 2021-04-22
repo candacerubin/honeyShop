1c1edf8b6ba213944fb2fc9ff48fe6b3945f3e1e
candacerubin <candacerubin0@gmail.com>
2021-04-21 20:07:05 -0700

    added backend routes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -56,7 +56,7 @@ const ProductScreen = ({ match }) => {
 									type='button'
 									disabled={product.countInStock === 0}
 								>
-									Add To Cart
+									{product.countInStock > 0 ? 'Add To Cart' : 'Out Of Stock'}
 								</Button>
 							</ListGroup.Item>
 						</ListGroup>
